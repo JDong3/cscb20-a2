c60a5f843d3ae591812849a93bb4e8ce8c3c0251
xxcandiez <josephdong1998@gmail.com>
2021-04-02 00:10:16 -0400

    xd

--- app.py
+++ app.py
@@ -2,39 +2,100 @@ import sqlite3
 from flask import Flask, render_template, session, request, redirect, url_for, g, current_app
 
 # TODO: hook up database, create db, put data, check login vs database
+# TODO: check login, retry error
 app = Flask(__name__)
+SESSION_KEYS = ['login_again', 'login_username', 'login_password',
+                'signup_user', 'signup_password_no_match', 'signup_good',
+                'signup_username', 'signup_password', 'signup_password_repeat']
+"""
+session variables
+    login_again: bool
+    signup_user_taken: bool, whether the username requested is taken
+    signup_password_no_match: bool
+    signup_good: bool
+"""
 
 @app.route('/')
 def main():
     if 'user' in session:
         return redirect('/index')
     else:
-        return render_template('login.html')
+        return redirect('/login')
 
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/trylogin', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        # check if username exists
-        rows = query('select * from users')
-        if request.form['username'] in [row['username'] for row in rows]:
-        session['user'] = request.form['username']
-        return redirect('/index')
-    if request.method == 'GET':
-        return render_template('login.html')
+    # check if username exists
+    authFailed = False
+    username = request.form['username']
+    password = request.form['password']
 
-def query(q):
+    session['login_username'] = username
+    session['login_password'] = password
+
+    users = query('select * from users where username = ?', [username])
+    if len(users) == 1 and users[0]['password'] == password:
+        session['user'] = username
+        return redirect('/index')
+    else:
+        print('here')
+        session['login_again'] = True
+        return main()
+
+@app.route('/trysignup', methods=['POST'])
+def signup():
+    username = request.form['username']
+    password = request.form['password']
+    password_repeat = request.form['passwordRepeat']
+    account_type = request.form['accountType']
+
+    user_taken = query('select * from users where username = ?', [username])
+    user_taken = len(user_taken) == 1
+    password_no_match = not password == password_repeat
+    empty_field = not (username and password and password_repeat)
+
+    session['signup_user_taken'] = user_taken
+    session['signup_password_no_match'] = password_no_match
+    session['signup_empty_field'] = empty_field
+    session['signup_username'] = username
+    session['signup_password'] = password
+    session['signup_password_repeat'] = password_repeat
+    session['signup_account_type'] = account_type
+
+    if user_taken or password_no_match or empty_field:
+        session['signup_good'] = False
+        return redirect('/signup')
+    else:
+        session['signup_good'] = True
+        insert('insert into users(username, password, type) values (?, ?, ?)', [username, password, account_type])
+        return redirect('/signup')
+
+def query(q, param):
     db = get_db()
     cur = db.cursor()
-    cur.execute(q)
+    cur.execute(q, param)
     return cur.fetchall()
+
+def insert(q, param):
+    db = get_db()
+    cur = db.cursor()
+    cur.execute(q, param)
+    db.commit()
+    return cur.lastrowid
 
 @app.route('/logout', methods=['POST'])
 def logout():
-    session.pop('user', None)
+    session.clear()
     return redirect('/login')
 
 @app.route('/<page>')
-def pages(page=None):
+def router(page=None):
+    fix_session()
+
+    if page == 'login':
+        return render_template('login.html', **session)
+    elif page == 'signup':
+        return render_template('signup.html', **session)
+
     if not 'user' in session:
         return redirect('/login')
 
@@ -52,6 +113,8 @@ def pages(page=None):
         return render_template('tests.html')
     elif page == 'tutorials':
         return render_template('tutorials.html')
+    elif page == 'signup':
+        return render_template('signup.html')
 
 def get_db():
     if not 'db' in g:
@@ -69,6 +132,11 @@ def close_db(e=None):
     if not db is None:
         db.close()
 
+
+def fix_session():
+    for key in SESSION_KEYS:
+        if not key in session:
+            session[key] = False
 
 app.teardown_appcontext(close_db)
 app.secret_key = 'applepen'
