aa119b7bafd755c8de8f24774e4bf2b6c005c27f
xxcandiez <josephdong1998@gmail.com>
2021-04-03 22:30:45 -0400

    xd

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 SESSION_KEYS = ['login_again', 'login_username', 'login_password',
                 'signup_user', 'signup_password_no_match', 'signup_good',
                 'signup_username', 'signup_password', 'signup_password_repeat',
-                'is_student']
+                'is_student', 'marks_marks', 'marks_do_remark', 'marks_remark_data', 'marks_marks_for', 'marks_do_answer',
+                'marks_students', 'feedback_feedback', 'feedback_instructors']
 """
 session variables
     login_again: bool
@@ -70,11 +71,19 @@ def signup():
         insert('insert into users(username, password, type) values (?, ?, ?)', [username, password, account_type])
         return redirect('/signup')
 
-def query(q, param):
+def query(q, param, extract=False):
     db = get_db()
     cur = db.cursor()
     cur.execute(q, param)
-    return cur.fetchall()
+    res = cur.fetchall()
+    if extract:
+        temp = [{} for re in res]
+        for i in range(len(res)):
+            for key in res[0].keys():
+                temp[i][key] = res[i][key]
+        return temp
+
+    return res
 
 def insert(q, param):
     db = get_db()
@@ -107,7 +116,7 @@ def router(page=None):
     elif page == 'calendar':
         return render_template('calendar.html', **session)
     elif page == 'feedback':
-        return render_template('feedback.html', **session)
+        return feedback()
     elif page == 'lectures':
         return render_template('lectures.html', **session)
     elif page == 'tests':
@@ -117,11 +126,110 @@ def router(page=None):
     elif page == 'signup':
         return render_template('signup.html', **session)
     elif page == 'marks':
-        return render_template('marks.html', **session)
+        return marks()
     elif page == 'manage' and session['is_student'] == False:
         return render_template('manage.html', **session)
     else:
         return 'What are you doing, please go back.'
+
+@app.route('/submitFeedback', methods=['POST'])
+def try_feedback():
+    instructor = request.form['instructor']
+    q1 = request.form['q1']
+    q2 = request.form['q2']
+    q3 = request.form['q3']
+    q4 = request.form['q4']
+    params = [instructor, q1, q2, q3, q4]
+    insert('insert into feedback(instructor, q1, q2, q3, q4) values (?, ?, ?, ?, ?)', params)
+    return redirect('/feedback')
+
+def feedback():
+    if not session['is_student']:
+        params = [session['user']]
+        your_feedback = query('select * from feedback where instructor = (?)', params, extract=True)
+        session['feedback_feedback'] = your_feedback
+    else:
+        instructors = query('select * from ships where student = (?)', [session['user']], extract=True)
+        session['feedback_instructors'] = instructors
+
+    return render_template('feedback.html', **session)
+
+@app.route('/submitRemark', methods=['POST'])
+def do_remark():
+    params = [request.form['rowid'], request.form['comment']]
+    session['marks_do_remark'] = False
+    insert('insert into requests (markid, comment) values (?, ?)', params)
+    return render_template('marks.html', **session)
+
+@app.route('/remark', methods=['POST'])
+def try_remark():
+    session['marks_do_remark'] = True
+    session['marks_remark_data'] = {
+        'rowid': request.form['rowid'],
+        'assignment': request.form['assignment']
+    }
+    return render_template('marks.html', **session)
+
+@app.route('/answer', methods=['POST'])
+def try_answer():
+    session['marks_do_answer'] = True
+    print("rowid", request.form['rowid'])
+    session['marks_answer_data'] = {
+        'rowid': request.form['rowid'],
+        'assignment': request.form['assignment'],
+        'student': request.form['student'],
+        'mark': request.form['mark']
+    }
+    return render_template('marks.html', **session)
+
+@app.route('/submitAnswer', methods=['POST'])
+def do_answer():
+    session['marks_do_answer'] = False
+
+    update_params = [request.form['mark'], request.form['rowid']]
+    # update mark
+    print('newmark:', request.form['mark'], request.form['rowid'])
+    insert('update marks set mark = (?) where rowid = (?)', update_params)
+    # remove remark request
+    insert('delete from requests where markid = (?)', [request.form['rowid']])
+    return render_template('marks.html', **session)
+
+@app.route('/changeMarkFor', methods=['POST'])
+def load_marks():
+    session['marks_marks_for'] = request.form['markFor']
+    return marks()
+
+def marks():
+    # case 1 student
+    if session['is_student']:
+        session['marks_marks_for'] = session['user']
+    elif not session['marks_marks_for']:
+        session['marks_marks_for'] = 'requests'
+
+    if not session['is_student']:
+        instructor = session['user']
+        students = query('select student from ships where instructor == (?)', [instructor], extract=True)
+        session['marks_students'] = [ele['student'] for ele in students]
+
+    if session['marks_marks_for'] == 'requests':
+        all_marks = query('select marks.rowid as rowid, assignment, mark, student, comment from requests cross join marks where requests.markid == marks.rowid;', [], extract=True)
+    else:
+        all_marks = query('select rowid, assignment, mark from marks where student = (?)', [session['marks_marks_for']], extract=True)
+
+    session['marks_marks'] = all_marks
+    return render_template('marks.html', **session)
+
+@app.route('/createNew', methods=["POST"])
+def try_create_new():
+    student = request.form['student']
+    assignment = request.form['assignment']
+    mark = request.form['mark']
+
+    params = [student, assignment, mark]
+
+    insert('insert into marks (student, assignment, mark) values (?, ?, ?)', params)
+
+    return marks()
 
 def get_db():
     if not 'db' in g:
@@ -138,7 +246,6 @@ def close_db(e=None):
 
     if not db is None:
         db.close()
-
 
 def fix_session():
     for key in SESSION_KEYS:
